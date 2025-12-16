a2d43ff28b47ce8febaea8e5155c9386d3aa5811
MohamDah <mohammedfaiseldhb@gmail.com>
2025-12-16 19:39:30 +0200

    Add BodyWrapper component for structured layout and update Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 'use client'
+import BodyWrapper from "./components/home/BodyWrapper";
 import HeroSection from "./components/home/HeroSection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-10 sm:p-16">
+    <main className="min-h-screen p-10 sm:p-16 ">
       <HeroSection />
-    </main>
+
+      <BodyWrapper>
+        <section className="py-12">
+          <h2 className="pb-4 text-3xl font-bold">Technologies</h2>
+        </section>
+      </BodyWrapper>
+
+    </main >
   );
 }
