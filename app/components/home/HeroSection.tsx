8a1fc97f46397663d3c0688c6e6d351a9f1e8ecd
MohamDah <mohammedfaiseldhb@gmail.com>
2025-12-16 18:15:46 +0200

    Add hero-fade-in animation to HeroSection for enhanced visual effect

--- app/components/home/HeroSection.tsx
+++ app/components/home/HeroSection.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 export default function HeroSection() {
   return (
-    <div className='sticky top-12 mx-auto h-full w-full max-w-7xl sm:top-16'>
+    <div className='sticky top-12 mx-auto h-full w-full max-w-7xl sm:top-16 hero-fade-in'>
       <div className='relative flex min-h-[calc(100dvh-5rem)] w-full flex-col sm:min-h-[calc(100dvh-8rem)] sm:justify-center sm:py-8'>
 
         <div className="flex flex-col gap-8">
@@ -24,8 +24,12 @@ export default function HeroSection() {
           <p className="prose text-justify sm:text-lg text-primary">I develop clean, performant solutions that solve real business problems. My work spans from crafting intuitive user interfaces to designing scalable backend systems. Check out my projects below to see how I&apos;ve helped companies ship better products faster.</p>
 
           <div className="flex items-center gap-3">
-            <GithubLogo className="size-10" />
-            <LinkedinLogo className="size-10" />
+            <a href="#" className='transition-transform hover:scale-110'>
+              <GithubLogo className="size-8.5" />
+            </a>
+            <a href="#" className='transition-transform hover:scale-110'>
+              <LinkedinLogo className="size-8.5" />
+            </a>
           </div>
         </div>
       </div>
